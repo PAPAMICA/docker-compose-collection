b60d91ea7cfe114f0205d9e469139157abaedf7b
Mickael Asseline <mickael@papamica.com>
2022-05-10 16:36:42 +0200

    Automation template (#1)
    * script automatique
    
    * essay template
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * add cicd
    
    * fix p
    
    * add valid message
    
    * test cicd
    
    * test cicd
    
    * test cicd
    
    * fix cicd
    
    * Automatic generation of the file templates-portainer.json
    
    * try fic
    
    * Automatic generation of the file templates-portainer.json
    
    * Update templates-portainer.json
    
    * Automatic generation of the file templates-portainer.json
    
    * try fix without cicd
    
    * try note
    
    * crossfinger
    
    * Automatic generation of the file templates-portainer.json
    
    * crossfinger x2
    
    * Automatic generation of the file templates-portainer.json
    
    * ajout description variable
    
    * Automatic generation of the file templates-portainer.json
    
    * remove test
    
    * Automatic generation of the file templates-portainer.json
    
    * add adminer
    
    * Automatic generation of the file templates-portainer.json
    
    * add bitwarden
    
    * Automatic generation of the file templates-portainer.json
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ generate-template-json.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+import glob
+import os
+import re
+import json
+from textwrap import indent
+
+GITHUB_REPOSITORY_URL = 'https://github.com/PAPAMICA/docker-compose-collection'
+try:
+    os.remove("./templates-portainer.json") 
+except:
+    print ("file don't exist")
+result = {}
+result["version"] = "2"
+result["templates"] = []
+templates = []
+
+for filename in glob.glob("*.y*ml"):
+    try:
+        data = []
+        dataset = {}
+        repository = {}
+        env = []
+        categories = []
+        file = open(filename)
+        for line in file.readlines():
+            if re.search('#&', line):
+                data=line[3:-1].split(': ', 1)
+                if data[0] == "type":
+                    type_=int(data[1])
+                    dataset[data[0]] = type_
+                elif data[0] == "categories":
+                    categories = data[1].split(', ')
+                    dataset[data[0]] = categories
+                else:
+                    dataset[data[0]] = data[1]
+            if re.search('#%', line):
+                envtemp = {}
+                dataenv=line[3:-1].split(': ', 1)
+                envtemp["name"] = dataenv[0]
+                envdesctotal = dataenv[1].split(' (')
+                envdesc = envdesctotal[1]
+                envtemp["label"] = envdesctotal[0]
+                envtemp["description"] = envdesc[:-1]
+                env.append(envtemp)
+        if data:
+            repository = {
+                "url": GITHUB_REPOSITORY_URL,
+                "stackfile": filename
+            }
+            dataset["repository"] = repository
+            #dataset["type"] = 3
+            dataset["env"] = env
+            templates.append(dataset)
+            print (f" ✅ {filename} ")
+            
+        else:
+            print (f" 🚸 {filename} not updated !")
+    except:
+         print (f" ❌ {filename} error !")
+
+try:
+    result["templates"] = templates
+    json_data = json.dumps(result, indent=4)
+    with open('templates-portainer.json', 'w') as outfile:
+        outfile.write(json_data)
+    print ("\n ✅ File generated !")
+except:
+    print (" ❌ Error when generate !")

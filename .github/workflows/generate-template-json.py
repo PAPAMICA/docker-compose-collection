0537edb69fda8a956cdd13e5837c9c3d3ffe9bf9
PAPAMICA <mickael@papamica.com>
2022-05-17 12:54:01 +0200

    try cicd

--- .github/workflows/generate-template-json.py
+++ .github/workflows/generate-template-json.py
@@ -43,7 +43,7 @@ for filename in sorted(glob.glob("composes-files/*.y*ml")):
                 elif data[0] == "note":
                     #link = re.search('\bhref=\'([^\']*)\'', data[1]).group(1)
                     dataset[data[0]] = data[1]
-                    print(link)
+                    #print(link)
                 elif data[0] == "title":
                     title = data[1]
                     dataset[data[0]] = title

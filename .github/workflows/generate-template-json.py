be087e949cbb8fdab0c6a53ceb1c2db37d58da5b
PAPAMICA <mickael@papamica.com>
2022-05-30 15:27:56 +0200

    try update reademe with url

--- .github/workflows/generate-template-json.py
+++ .github/workflows/generate-template-json.py
@@ -44,7 +44,7 @@ for filename in sorted(glob.glob("composes-files/*.y*ml")):
                 elif data[0] == "note":
                     try:
                         link = re.search("(?<=href=')[^']+(?=')", data[1]).group()
-                        website = re.search("(?<=rel='noopener'>)[^']+(?=<)", data[1]).group()
+                        website = re.search("(?<=rel='noopener'>)[^']+(?=<\/a)", data[1]).group()
                     except:
                         link=""
                         website=""
@@ -93,7 +93,7 @@ for filename in sorted(glob.glob("composes-files/*.y*ml")):
             dataset["env"] = env
             templates.append(dataset)
             print (f" ✅ {filename} ")
-            SERVICES=SERVICES + f'\n| ✅ | <img src="{logo}" alt="{file_name}" width="20"/> [{file_name}](https://github.com/PAPAMICA/docker-compose-collection/tree/master/composes-files/{file_name}.yml) | [{website}]{link} | {date} | {maintainer} |'
+            SERVICES=SERVICES + f'\n| ✅ | <img src="{logo}" alt="{file_name}" width="20"/> [{file_name}](https://github.com/PAPAMICA/docker-compose-collection/tree/master/composes-files/{file_name}.yml) | [{website}]({link}) | {date} | {maintainer} |'
             nb_a += 1
             
         else:

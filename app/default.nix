c60b2a2d11e8b4ee3effcb3a6f17ff0680f5e68f
QJoly <qjoly@users.noreply.github.com>
2022-10-16 11:17:34 +0200

    Fichier Nix avec dépendances

---
+++ app/default.nix
@@ -0,0 +1,15 @@
+# default.nix
+with (import <nixpkgs> {});
+let
+  python-packages = python-packages: with python-packages; [
+    requests
+    jinja2
+  ];
+  python-with-packages = python39.withPackages python-packages;
+in
+mkShell {
+  buildInputs = [
+    python-with-packages
+    python39Packages.pip
+  ];
+}

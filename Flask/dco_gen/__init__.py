94656332796c50dd551ab5c73af994832d2935c1
mouriermaxime <58846521+mouriermaxime@users.noreply.github.com>
2023-01-14 15:21:17 +0100

    render page

--- Flask/dco_gen/__init__.py
+++ Flask/dco_gen/__init__.py
@@ -1,2 +1,3 @@
 from flask import Flask
 from .views import app
+from . import config
--- Flask/dco_gen/views.py
+++ Flask/dco_gen/views.py
@@ -1,3 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'iVu"D[%!N*ZL18K)s\rSW=@:s'
+
+@app.route('/generator/interactif',methods = ['GET','POST'])
+@app.route('/generator/interactif/',methods = ['GET','POST'])
+def generate_compose():
+    return render_template('index.html',connard="Micka")
+
+if __name__ == "__main__":
+    app.run()
